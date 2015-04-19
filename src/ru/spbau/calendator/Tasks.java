5ddf8c4905420b46324edd41269e548df86dd359
LabutinIgor <igorl@MacBook-Air-Igor.local>
2015-04-19 14:43:17 +0300

    Made reading tasks from jsons

--- src/ru/spbau/calendator/Tasks.java
+++ src/ru/spbau/calendator/Tasks.java
@@ -1,24 +1,41 @@
 package ru.spbau.calendator;
 
 
-import net.fortuna.ical4j.model.Calendar;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.File;
+import java.io.FileReader;
 
 public class Tasks {
     public static void run() throws Exception {
-        Tools.get_cal_from_url("https://www.google.com/calendar/ical/appirio.com_bhga3musitat85mhdrng9035jg%40group.calendar.google.com/public/basic.ics", "topcoder.ics");
-        Tools.get_cal_from_url("https://www.google.com/calendar/ical/br1o1n70iqgrrbc875vcehacjg%40group.calendar.google.com/public/basic.ics", "codeforces.ics");
-
-        Calendar topcoder = Tools.read_calendar("topcoder.ics");
-        Calendar codeforces = Tools.read_calendar("codeforces.ics");
-        Calendar srms = Tools.filter_by_name("^[\\s\\S]*SRM[\\s\\S]*$", topcoder);
-        Calendar div1 = Tools.filter_by_name("^[\\s\\S]*Div\\. 1[\\s\\S]*$", codeforces);
-
-        Calendar srms_cf_div1 = Tools.merge_cals(srms, div1);
-        Tools.print_calendar(srms_cf_div1, "srms&cf_div1.ics");
-
-        Calendar cf_start_end_long_events = Tools.long_events_to_bounds(codeforces, 2*24*60*60*1000);
-        Tools.print_calendar(cf_start_end_long_events, "cf_start_end_long_events.ics");
+        File F = new File("data/queries/");
+        File []files = F.listFiles();
+        for (File file : files) {
+            JSONParser parser = new JSONParser();
+            FileReader reader = new FileReader(file);
+            JSONObject json = (JSONObject) parser.parse(reader);
+            make_cal_from_json(json);
+            reader.close();
+        }
     }
 
+    public static void make_cal_from_json(JSONObject json) throws Exception{
+        String type = (String) json.get("type");
+        if (type.equals("get_from_url"))
+            Tools.get_cal_from_url((String) json.get("url"), (String) json.get("output_cal_name"));
+
+        if (type.equals("name_filter"))
+            Tools.print_calendar(Tools.filter_by_name((String) json.get("pattern"), Tools.read_calendar((String) json.get("input_cal_name"))),
+                    (String) json.get("output_cal_name"));
+
+        if (type.equals("merge"))
+            Tools.print_calendar(Tools.merge_cals(Tools.read_calendar((String) json.get("cal1")), Tools.read_calendar((String) json.get("cal2"))),
+                    (String) json.get("output_cal_name"));
+
+        if (type.equals("long_to_bounds"))
+            Tools.print_calendar(Tools.long_events_to_bounds(Tools.read_calendar((String) json.get("input_cal_name")), (Long) json.get("time")),
+                    (String) json.get("output_cal_name"));
+    }
 
 }
