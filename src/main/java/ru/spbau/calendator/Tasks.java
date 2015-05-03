060a6a6032deaa91b056e884f7017ac3218877c5
LabutinIgor <Labutin.IgorL@gmail.com>
2015-05-03 19:57:53 +0300

    made default names and "disabled" jsons

--- src/main/java/ru/spbau/calendator/Tasks.java
+++ src/main/java/ru/spbau/calendator/Tasks.java
@@ -15,27 +15,36 @@ public class Tasks {
             JSONParser parser = new JSONParser();
             FileReader reader = new FileReader(file);
             JSONObject json = (JSONObject) parser.parse(reader);
-            make_cal_from_json(json);
+            make_cal_from_json(json, file.getName());
             reader.close();
         }
     }
 
-    public static void make_cal_from_json(JSONObject json) throws Exception{
+    public static void make_cal_from_json(JSONObject json, String file_name) throws Exception {
+        if (json.containsKey("disabled") && (Boolean) json.get("disabled")) {
+            return;
+        }
         String type = (String) json.get("type");
+        String out_cal_name;
+        if (json.containsKey("output_cal_name")) {
+             out_cal_name = (String) json.get("output_cal_name");
+        } else {
+            out_cal_name = file_name + ".ics";
+        }
         if (type.equals("get_from_url"))
-            Tools.get_cal_from_url((String) json.get("url"), (String) json.get("output_cal_name"));
+            Tools.get_cal_from_url((String) json.get("url"), out_cal_name);
 
         if (type.equals("name_filter"))
             Tools.print_calendar(Tools.filter_by_name((String) json.get("pattern"), Tools.read_calendar((String) json.get("input_cal_name"))),
-                    (String) json.get("output_cal_name"));
+                    out_cal_name);
 
         if (type.equals("merge"))
             Tools.print_calendar(Tools.merge_cals(Tools.read_calendar((String) json.get("cal1")), Tools.read_calendar((String) json.get("cal2"))),
-                    (String) json.get("output_cal_name"));
+                    out_cal_name);
 
         if (type.equals("long_to_bounds"))
             Tools.print_calendar(Tools.long_events_to_bounds(Tools.read_calendar((String) json.get("input_cal_name")), (Long) json.get("time")),
-                    (String) json.get("output_cal_name"));
+                    out_cal_name);
     }
 
 }
