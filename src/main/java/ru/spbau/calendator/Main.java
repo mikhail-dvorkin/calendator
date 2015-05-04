edb0f2bafc50c15e61d0c84b2a087824fc190fa6
LabutinIgor <Labutin.IgorL@gmail.com>
2015-05-04 20:38:06 +0300

    some logging and catching exceptions

--- src/main/java/ru/spbau/calendator/Main.java
+++ src/main/java/ru/spbau/calendator/Main.java
@@ -1,13 +1,35 @@
 package ru.spbau.calendator;
 
 
+import java.io.IOException;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Main {
+    public static Logger logger;
+    static {
+        try {
+            FileHandler fh = new FileHandler("log/all_log.log", true);
+            fh.setFormatter(new SimpleFormatter());
+            logger = Logger.getLogger("Logger");
+            logger.addHandler(fh);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         while (true) {
-            Tasks.run();
-            //System.out.println("UPDATED");
+            try {
+                Tasks.run();
+                logger.info("Calendars updated");
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
             TimeUnit.MINUTES.sleep(1);
         }
     }
--- src/main/java/ru/spbau/calendator/Tasks.java
+++ src/main/java/ru/spbau/calendator/Tasks.java
@@ -12,52 +12,60 @@ public class Tasks {
     public static void run() throws Exception {
         File F = new File("data/queries/");
         File []files = F.listFiles();
-        for (File file : files) {
-            JSONParser parser = new JSONParser();
-            FileReader reader = new FileReader(file);
-            JSONObject json = (JSONObject) parser.parse(reader);
-            make_cal_from_json(json, file.getName());
-            reader.close();
+        if (files == null) {
+            Main.logger.warning("No json queries found");
+        } else {
+            for (File file : files) {
+                JSONParser parser = new JSONParser();
+                FileReader reader = new FileReader(file);
+                JSONObject json = (JSONObject) parser.parse(reader);
+                make_cal_from_json(json, file.getName());
+                reader.close();
+            }
         }
     }
 
     public static void make_cal_from_json(JSONObject json, String file_name) throws Exception {
-        if (json.containsKey("disabled") && (Boolean) json.get("disabled")) {
-            return;
-        }
-        String type = (String) json.get("type");
-        String out_cal_name;
-        if (json.containsKey("output_cal_name")) {
-             out_cal_name = (String) json.get("output_cal_name");
-        } else {
-            out_cal_name = file_name + ".ics";
-        }
-        if (type.equals("get_from_url"))
-            Tools.get_cal_from_url((String) json.get("url"), out_cal_name);
+        try {
+            if (json.containsKey("disabled") && (Boolean) json.get("disabled")) {
+                return;
+            }
+            String type = (String) json.get("type");
+            String out_cal_name;
+            if (json.containsKey("output_cal_name")) {
+                out_cal_name = (String) json.get("output_cal_name");
+            } else {
+                out_cal_name = file_name + ".ics";
+            }
+            if (type.equals("get_from_url"))
+                Tools.get_cal_from_url((String) json.get("url"), out_cal_name);
 
-        if (type.equals("name_filter"))
-            Tools.print_calendar(Tools.filter_by_name((String) json.get("pattern"), Tools.read_calendar((String) json.get("input_cal_name"))),
-                    out_cal_name);
+            if (type.equals("name_filter"))
+                Tools.print_calendar(Tools.filter_by_name((String) json.get("pattern"), Tools.read_calendar((String) json.get("input_cal_name"))),
+                        out_cal_name);
 
-        if (type.equals("merge"))
-            Tools.print_calendar(Tools.merge_cals(Tools.read_calendar((String) json.get("cal1")), Tools.read_calendar((String) json.get("cal2"))),
-                    out_cal_name);
+            if (type.equals("merge"))
+                Tools.print_calendar(Tools.merge_cals(Tools.read_calendar((String) json.get("cal1")), Tools.read_calendar((String) json.get("cal2"))),
+                        out_cal_name);
 
-        if (type.equals("long_to_bounds"))
-            Tools.print_calendar(Tools.long_events_to_bounds(Tools.read_calendar((String) json.get("input_cal_name")), (Long) json.get("time")),
-                    out_cal_name);
+            if (type.equals("long_to_bounds"))
+                Tools.print_calendar(Tools.long_events_to_bounds(Tools.read_calendar((String) json.get("input_cal_name")), (Long) json.get("time")),
+                        out_cal_name);
 
-        if (type.equals("parse")) {
-            Class cls = Class.forName((String) json.get("path"));
-            Parser parser = (Parser) cls.newInstance();
-            Tools.print_calendar(parser.parse(),
-                    out_cal_name);
-        }
-        if (type.equals("process")) {
-            Class cls = Class.forName((String) json.get("path"));
-            Processor processor = (Processor) cls.newInstance();
-            Tools.print_calendar(processor.process(((JSONArray) json.get("args")).toArray()),
-                    out_cal_name);
+            if (type.equals("parse")) {
+                Class cls = Class.forName((String) json.get("path"));
+                Parser parser = (Parser) cls.newInstance();
+                Tools.print_calendar(parser.parse(),
+                        out_cal_name);
+            }
+            if (type.equals("process")) {
+                Class cls = Class.forName((String) json.get("path"));
+                Processor processor = (Processor) cls.newInstance();
+                Tools.print_calendar(processor.process(((JSONArray) json.get("args")).toArray()),
+                        out_cal_name);
+            }
+        } catch (Exception ex) {
+            Main.logger.warning("Error in json " + file_name);
         }
     }
 
